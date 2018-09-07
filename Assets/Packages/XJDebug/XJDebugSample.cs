db4ae39cf1ef04332edb0c1d1c21f5836d0c6bc2
XJINE <xjine.andorelseif@gmail.com>
2018-09-07 12:22:16 +0900

    Add unitypackage & cs sources.

--- Assets/Packages/XJDebug/XJDebugSample.cs
+++ Assets/Packages/XJDebug/XJDebugSample.cs
@@ -7,38 +7,19 @@ public class XJDebugSample : MonoBehaviour
     void Start ()
     {
         // SAMPLE:1
-        // Log are able to control with tag.
+        // Log are able to control with tag. Default tag value is null and it is enabled.
+        // You can switch the setting with XJDebug.NullIsEnableTag if you need.
+        // XJDebug.NullIsEnableTag = true;
 
         XJDebug.EnableTag("TAG_A");
         XJDebug.DisableTag("TAG_B");
 
         XJDebug.Log("MESSAGE_1", "TAG_A");
         XJDebug.Log("MESSAGE_2", this, "TAG_B");
+        XJDebug.LogWarning("MESSAGE_3", this);
+
 
         // SAMPLE:2
-        // If tag is omitted, it becomes null.
-        // null is enabled tag in default, but able to switch the setting.
-
-        XJDebug.Log("MESSAGE_3", this);
-        XJDebug.Log("MESSAGE_4", this, null);
-
-        XJDebug.NullIsEnableTag = false;
-
-        XJDebug.Log("MESSAGE_5");
-
-        XJDebug.NullIsEnableTag = true;
-
-        // SAMPLE:3
-        // null message or context shows "NULL".
-
-        XJDebug.Log(null);
-
-        // SAMPLE:4
-        // There are logTypes such as Warning, Exception or any others.
-        
-        XJDebug.LogWarning("MESSAGE_6");
-
-        // SAMPLE:5
         // Output format is able to set with "XJDebug.LogInfo.StringFormat".
         // 
         // {0} : LogType.
@@ -49,26 +30,18 @@ public class XJDebugSample : MonoBehaviour
         // {5} : Tag.
 
         XJDebug.LogInfo.StringFormat = "{2}({1}) {3}";
+        XJDebug.Log("MESSAGE_4");
 
-        XJDebug.Log("MESSAGE_13");
-
-        // SAMPLE:6
+        // SAMPLE:3
         // ICollection such as Array, List or any others are splitted to each item.
         // Splitter format is able to set with "XJDebug.LogInfo.StringFormatSplitter".
+        // XJDebug.LogInfo.StringFormatSplitter = "/";
 
-        string[] stringArray = new string[]{ "MESSAGE_7", "MESSAGE_8", "MESSAGE_9" };
-        XJDebug.Log(stringArray);
-
-        XJDebug.LogInfo.StringFormatSplitter = "/";
-
-        List<int> intList = new List<int>() { 10, 11, 12 };
+        List<int> intList = new List<int>() { 5, 6, 7 };
         XJDebug.Log(intList);
 
-        // SAMPLE:7
+        // SAMPLE:4
         // Logs are keeped in XJDebug.Logs upto XJDebug.MaxLogCount.
-        // Related settings are there.
-        // - XJDebug.KeepDisableTagLog
-        // - XJDebug.MaxLogCount
 
         foreach (XJDebug.LogInfo log in XJDebug.Logs.Where<XJDebug.LogInfo>(log => log.tag == null))
         {
---
+++ XJDebug.cs
@@ -0,0 +1,225 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+public static partial class XJDebug
+{
+    // WARNING:
+    // This utility is must be used as dll.
+    // If you need more detail, check comments in "XJDebug.Log".
+
+    // NOTE:
+    // This utility doesn't consider any rich text in l(L)ogs.
+
+    #region Field
+
+    // NOTE:
+    // To make a ReadOnlyCollection, it need to use List instead of Queue.
+    // Because Queue does't inherit IList.
+
+    public static bool KeepDisableTagLog = false;
+
+    public static bool NullIsEnableTag = true;
+
+    private static int maxLogCount = 1000;
+
+    private static List<LogInfo> logs;
+
+    private static Dictionary<string, bool> tags;
+
+    #endregion Field
+
+    #region Property
+
+    public static int MaxLogCount
+    {
+        get
+        {
+            return XJDebug.maxLogCount;
+        }
+        set
+        {
+            XJDebug.maxLogCount = value;
+            TrimLogs();
+        }
+    }
+
+    public static ReadOnlyCollection<LogInfo> Logs
+    {
+        get;
+        private set;
+    }
+
+    public static ReadOnlyDictionary<string, bool> Tags 
+    {
+        get;
+        private set;
+    }
+
+    #endregion Property
+
+    #region Constructor
+
+    static XJDebug()
+    {
+        XJDebug.logs = new List<XJDebug.LogInfo>();
+        XJDebug.Logs = new ReadOnlyCollection<XJDebug.LogInfo>(XJDebug.logs);
+
+        XJDebug.tags = new Dictionary<string, bool>();
+        XJDebug.Tags = new ReadOnlyDictionary<string, bool>(XJDebug.tags);
+    }
+
+    #endregion Constructor
+
+    #region Method
+
+    public static void EnableTag(string tag)
+    {
+        if (XJDebug.tags.ContainsKey(tag))
+        {
+            XJDebug.tags[tag] = true;
+        }
+        else
+        {
+            XJDebug.tags.Add(tag, true);
+        }
+    }
+
+    public static void DisableTag(string tag)
+    {
+        if (XJDebug.tags.ContainsKey(tag))
+        {
+            XJDebug.tags[tag] = false;
+        }
+        else
+        {
+            XJDebug.tags.Add(tag, false);
+        }
+    }
+
+    public static bool TagIsEnable(string tag)
+    {
+        if (tag == null)
+        {
+            return XJDebug.NullIsEnableTag;
+        }
+
+        if (XJDebug.tags.ContainsKey(tag))
+        {
+            return XJDebug.tags[tag];
+        }
+
+        return false;
+    }
+
+    public static void LogError(object message, string tag = null)
+    {
+        LogError(message, null, tag);
+    }
+
+    public static void LogError(object message, Object context, string tag = null)
+    {
+        Log(LogType.Error, tag, message, context);
+    }
+
+    public static void LogAssert(object message, string tag = null)
+    {
+        LogAssert(message, null, tag);
+    }
+
+    public static void LogAssert(object message, Object context, string tag = null)
+    {
+        Log(LogType.Assert, tag, message, context);
+    }
+
+    public static void LogWarning(object message, string tag = null)
+    {
+        LogWarning(message, null, tag);
+    }
+
+    public static void LogWarning(object message, Object context, string tag = null)
+    {
+        Log(LogType.Warning, tag, message, context);
+    }
+
+    public static void LogException(object message, string tag = null)
+    {
+        LogException(message, null, tag);
+    }
+
+    public static void LogException(object message, Object context, string tag = null)
+    {
+        Log(LogType.Exception, tag, message, context);
+    }
+
+    public static void Log(object message, string tag = null)
+    {
+        Log(message, null, tag);
+    }
+
+    public static void Log(object message, Object context, string tag = null)
+    {
+        Log(LogType.Log, tag, message, context);
+    }
+
+    private static void Log(LogType logType, string tag, object message, Object context)
+    {
+        // WARNING:
+        // When use standard UnityEditor & Debug.Log,
+        // we can jump to the call-point in source code with console click.
+        // However in this case, the call-point becomming following "Debug.unityLogger.Log".
+        // Unfortunately, there are no solution to avoid this problem from any code.
+        // So we have to make a ".dll".
+        // https://answers.unity.com/questions/176422/debug-wrapper-class.html
+        // https://answers.unity.com/questions/1226230/how-to-properly-call-debuglogstring-in-a-custom-lo.html
+
+        // NOTE:
+        // Arguments order is same as "Debug.unity.logger.Log".
+
+        LogInfo logInfo = new LogInfo(logType, message, context, tag);
+
+        bool tagIsEnable = TagIsEnable(tag);
+
+        if (tagIsEnable)
+        {
+            Debug.unityLogger.Log(logType, tag, logInfo.ToString(), context);
+        }
+
+        if (tagIsEnable || XJDebug.KeepDisableTagLog)
+        {
+            UpdateLogs(logInfo);
+        }
+    }
+
+    private static void UpdateLogs(LogInfo logInfo) 
+    {
+        XJDebug.logs.Add(logInfo);
+        TrimLogs();
+    }
+
+    private static void TrimLogs() 
+    {
+        int logsCount = XJDebug.logs.Count;
+
+        while (logsCount > XJDebug.maxLogCount)
+        {
+            XJDebug.logs.RemoveAt(0);
+            logsCount = XJDebug.logs.Count;
+        }
+    }
+
+    public static void ClearLogs() 
+    {
+        XJDebug.logs.Clear();
+    }
+
+    public static void ClearTags() 
+    {
+        // NOTE:
+        // All of tags will shows disable.
+
+        XJDebug.tags.Clear();
+    }
+
+    #endregion Method
+}
